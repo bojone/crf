3ef17e8379684b120d3cc237a66d2b1606a3c17c
苏剑林 <bojone@spaces.ac.cn>
2018-05-18 13:00:39 +0800

    Create crf_keras.py

---
+++ crf_keras.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*-
+
+from keras.layers import Layer
+import keras.backend as K
+
+
+class CRF(Layer):
+    """纯Keras实现CRF层
+    CRF层本质上是一个带训练参数的loss计算层，因此CRF层只用来训练模型，
+    而预测则需要另外建立模型。
+    """
+    def __init__(self, ignore_last_label=False, **kwargs):
+        """ignore_last_label：定义要不要忽略最后一个标签，起到mask的效果
+        """
+        self.ignore_last_label = 1 if ignore_last_label else 0
+        super(CRF, self).__init__(**kwargs)
+    def build(self, input_shape):
+        self.num_labels = input_shape[0][-1] - self.ignore_last_label
+        self.trans = self.add_weight(name='crf_trans',
+                                     shape=(self.num_labels, self.num_labels),
+                                     initializer='glorot_uniform',
+                                     trainable=True)
+    def log_norm_step(self, inputs, states):
+        """递归计算归一化因子
+        要点：1、递归计算；2、用logsumexp避免溢出。
+        技巧：通过expand_dims来对齐张量。
+        """
+        states = K.expand_dims(states[0], 2) # (batch_size, output_dim, 1)
+        trans = K.expand_dims(self.trans, 0) # (1, output_dim, output_dim)
+        output = K.logsumexp(states+trans, 1) # (batch_size, output_dim)
+        return output+inputs, [output+inputs]
+    def log_path_proba(self, inputs, labels):
+        """计算目标路径的相对概率（还没有归一化）
+        要点：逐标签得分，加上转移概率得分。
+        技巧：用“预测”点乘“目标”的方法抽取出目标路径的得分。
+        """
+        point_proba = K.sum(K.sum(inputs*labels, 2), 1, keepdims=True) # 逐标签得分
+        labels1 = K.expand_dims(labels[:, :-1], 3)
+        labels2 = K.expand_dims(labels[:, 1:], 2)
+        labels = labels1 * labels2 # 两个错位labels，负责从转移矩阵中抽取目标转移得分
+        trans = K.expand_dims(K.expand_dims(self.trans, 0), 0)
+        trans_proba = K.sum(K.sum(trans*labels, [2,3]), 1, keepdims=True)
+        return point_proba+trans_proba # 两部分得分之和
+    def call(self, inputs):
+        inputs,labels = inputs # 以“预测值+目标(one hot)”为输入
+        mask = 1-labels[:,:,-1] if self.ignore_last_label else None
+        inputs,labels = inputs[:,:,:self.num_labels],labels[:,:,:self.num_labels]
+        init_states = [inputs[:,0]] # 初始状态
+        log_norm,_,_ = K.rnn(self.log_norm_step, inputs[:,1:], init_states, mask=mask) # 计算Z向量（对数）
+        log_norm = K.logsumexp(log_norm, 1, keepdims=True) # 计算Z（对数）
+        log_path_proba = self.log_path_proba(inputs, labels) # 计算分子（对数）
+        return log_norm - log_path_proba # 即log(分子/分母)
