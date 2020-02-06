6bd55d9136d9430cf4c04cfee3c1b288dbe01628
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2020-02-06 13:34:28 +0800

    Update crf_keras.py

--- crf_keras.py
+++ crf_keras.py
@@ -14,12 +14,14 @@ class CRF(Layer):
         """
         self.ignore_last_label = 1 if ignore_last_label else 0
         super(CRF, self).__init__(**kwargs)
+
     def build(self, input_shape):
         self.num_labels = input_shape[-1] - self.ignore_last_label
         self.trans = self.add_weight(name='crf_trans',
                                      shape=(self.num_labels, self.num_labels),
                                      initializer='glorot_uniform',
                                      trainable=True)
+
     def log_norm_step(self, inputs, states):
         """递归计算归一化因子
         要点：1、递归计算；2、用logsumexp避免溢出。
@@ -32,38 +34,42 @@ class CRF(Layer):
         outputs = outputs + inputs
         outputs = mask * outputs + (1 - mask) * states[:, :, 0]
         return outputs, [outputs]
+
     def path_score(self, inputs, labels):
         """计算目标路径的相对概率（还没有归一化）
         要点：逐标签得分，加上转移概率得分。
         技巧：用“预测”点乘“目标”的方法抽取出目标路径的得分。
         """
-        point_score = K.sum(K.sum(inputs*labels, 2), 1, keepdims=True) # 逐标签得分
+        point_score = K.sum(K.sum(inputs * labels, 2), 1, keepdims=True)  # 逐标签得分
         labels1 = K.expand_dims(labels[:, :-1], 3)
         labels2 = K.expand_dims(labels[:, 1:], 2)
-        labels = labels1 * labels2 # 两个错位labels，负责从转移矩阵中抽取目标转移得分
+        labels = labels1 * labels2  # 两个错位labels，负责从转移矩阵中抽取目标转移得分
         trans = K.expand_dims(K.expand_dims(self.trans, 0), 0)
-        trans_score = K.sum(K.sum(trans*labels, [2,3]), 1, keepdims=True)
-        return point_score+trans_score # 两部分得分之和
-    def call(self, inputs): # CRF本身不改变输出，它只是一个loss
+        trans_score = K.sum(K.sum(trans * labels, [2, 3]), 1, keepdims=True)
+        return point_score + trans_score  # 两部分得分之和
+
+    def call(self, inputs):  # CRF本身不改变输出，它只是一个loss
         return inputs
-    def loss(self, y_true, y_pred): # 目标y_pred需要是one hot形式
+
+    def loss(self, y_true, y_pred):  # 目标y_pred需要是one hot形式
         if self.ignore_last_label:
-            mask = 1-y_true[:,:,-1:]
+            mask = 1 - y_true[:, :, -1:]
         else:
-            mask = K.ones_like(y_pred[:,:,:1])
-        y_true,y_pred = y_true[:,:,:self.num_labels],y_pred[:,:,:self.num_labels]
-        path_score = self.path_score(y_pred, y_true) # 计算分子（对数）
-        init_states = [y_pred[:,0]] # 初始状态
+            mask = K.ones_like(y_pred[:, :, :1])
+        y_true, y_pred = y_true[:, :, :self.num_labels], y_pred[:, :, :self.num_labels]
+        path_score = self.path_score(y_pred, y_true)  # 计算分子（对数）
+        init_states = [y_pred[:, 0]]  # 初始状态
         y_pred = K.concatenate([y_pred, mask])
-        log_norm,_,_ = K.rnn(self.log_norm_step, y_pred[:,1:], init_states) # 计算Z向量（对数）
-        log_norm = K.logsumexp(log_norm, 1, keepdims=True) # 计算Z（对数）
-        return log_norm - path_score # 即log(分子/分母)
-    def accuracy(self, y_true, y_pred): # 训练过程中显示逐帧准确率的函数，排除了mask的影响
-        mask = 1-y_true[:,:,-1] if self.ignore_last_label else None
-        y_true,y_pred = y_true[:,:,:self.num_labels],y_pred[:,:,:self.num_labels]
+        log_norm, _, _ = K.rnn(self.log_norm_step, y_pred[:, 1:], init_states)  # 计算Z向量（对数）
+        log_norm = K.logsumexp(log_norm, 1, keepdims=True)  # 计算Z（对数）
+        return log_norm - path_score  # 即log(分子/分母)
+
+    def accuracy(self, y_true, y_pred):  # 训练过程中显示逐帧准确率的函数，排除了mask的影响
+        mask = 1 - y_true[:, :, -1] if self.ignore_last_label else None
+        y_true, y_pred = y_true[:, :, :self.num_labels], y_pred[:, :, :self.num_labels]
         isequal = K.equal(K.argmax(y_true, 2), K.argmax(y_pred, 2))
         isequal = K.cast(isequal, 'float32')
         if mask == None:
             return K.mean(isequal)
         else:
-            return K.sum(isequal*mask) / K.sum(mask)
+            return K.sum(isequal * mask) / K.sum(mask)
